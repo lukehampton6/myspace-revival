cc241f442d619cd7fb2eb0334d001ebf256576a4
Luke Hampton <lukehampton6@gmail.com>
2021-08-20 12:05:20 -0400

    file structure + user model

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model, Types } = require("mongoose");
+
+const UserSchema = new Schema(
+    {
+        username:{
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
